078d8a7ee17640397a72372e51022d83bec39bda
Sean Kapus <srkapus@hotmail.com>
2014-08-04 16:48:25 -0700

    snake game
    first snake

---
+++ snake/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+
+from django.contrib import admin
+from snake import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'snake.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'gamer.views.home', name='home'),
+)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
